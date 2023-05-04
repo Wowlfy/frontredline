042e6ec5ec1fe085f9b0e5b94a406d0efac4b4f5
Wowlfy <maxime.giraudeau@hotmail.fr>
2023-05-04 16:31:14 +0200

    maj

--- index.js
+++ index.js
@@ -7,3 +7,34 @@ axios.get('https://jsonplaceholder.typicode.com/posts')
     .catch(error => {
         console.error(error);
     });
+import axios from 'axios'
+const instance = axios.create({
+    baseURL: 'https://example.com',
+    headers: {
+        'content-type':'application/octet-stream',
+        'x-rapidapi-host':'example.com',
+        'x-rapidapi-key': process.env.RAPIDAPI_KEY
+    },
+});
+export default {
+    getData: () =>
+        instance({
+            'method':'GET',
+            'url':'/query',
+            'params': {
+                'search':'parameter',
+            },
+        }),
+    postData: () =>
+        instance({
+            'method': 'POST',
+            'url':'/api',
+            'data': {
+                'item1':'data1',
+                'item2':'item2'
+            },
+            'headers': {
+                'content-type':'application/json'  // override instance defaults
+            }
+        })
+}
