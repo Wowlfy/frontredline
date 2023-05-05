777d3d26695633d21689d1ff0a34d6bc88ff05d7
Oozogul <onur.o@emiles.fr>
2023-05-05 09:59:56 +0200

    Ajout du header dans la page principale

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include "header.php" ?>
+
+<div class="container">
+<div class="first-part">
+
+    <div class="promo-infos">
+        <label for="">Promo : </label>
+        <a href="">CDA2022-01</a>
+    </div>
+
+    <div class="promo-dates">
+        <span>Période du X au X</span>
+    </div>
+
+</div>
+<div class="second-part">
+
+
+</div>
+</div>
+
+
